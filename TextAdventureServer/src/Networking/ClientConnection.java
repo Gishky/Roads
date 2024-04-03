05de9387e2eb55c0d0fac7a46938b57bc6be70cd
Gishky <bischtus@gmail.com>
2024-04-03 17:40:02 +0200

    Refactor to easy game to get back into it

--- TextAdventureServer/src/Networking/ClientConnection.java
+++ TextAdventureServer/src/Networking/ClientConnection.java
@@ -4,30 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import Player.Player;
-import Server.MainThread;
-
 public class ClientConnection extends Thread {
     private PrintWriter out;
     private BufferedReader in;
     
-    private Player player = null;
-
     public void run(){
     	String inputLine;
     	try {
         	inputLine = in.readLine();
 			while((inputLine = in.readLine()) != null) {
-				if(player == null) {
-					String name = inputLine.split(";")[0];
-					String password = inputLine.split(";")[1];
-					if((player = MainThread.getPlayer(name, password)) == null) {
-						player = new Player(name,password);
-						MainThread.createPlayer(player);
-					}
-				}else {
-					player.messageReceived(inputLine);
-				}
+				
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- TextAdventureServer/src/Server/MainThread.java
+++ TextAdventureServer/src/Server/MainThread.java
@@ -1,30 +1,12 @@
 package Server;
 
-import java.util.ArrayList;
-
 import Networking.PortListener;
-import Player.Player;
 
 public class MainThread {
-	
-	private static ArrayList<Player> players = new ArrayList<Player>();
 
 	public static void main(String[] args) {
 		PortListener listener = new PortListener(61852);
 		listener.start();
 	}
 	
-	public static void createPlayer(Player p) {
-		players.add(p);
-	}
-	
-	public static Player getPlayer(String name, String password) {
-		for(Player p: players) {
-			if(p.getName().equals(name) && p.getPassword().equals(password)) {
-				return p;
-			}
-		}
-		
-		return null;
-	}
 }
